057c12a536c75d8476cb9cbd70e5142d0cec9aa9
abramceastham-byte <abramceastham@gmail.com>
2025-09-03 17:45:07 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Abram Eastham>
+ *        Date: <9/3/25>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
